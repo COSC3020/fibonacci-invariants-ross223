675818fc12fd0b548b7abcbc6332bdfac8925218
ross223 <144744308+ross223@users.noreply.github.com>
2024-03-03 15:52:38 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
 function fib(n) {
-    return n;
+    var tmp = [0, 1];
+    var i = 2;
+    var a = logic(n, tmp, i);
+    return a;
+}
+
+function logic(n, tmp, i){
+    if (n == 0){
+        tmp = [0];
+        return tmp;
+    } else if(n == 1){
+        return tmp;
+    } else {
+        if (i > n){
+            return tmp;
+        } else {
+            tmp[i] = tmp[i - 1] + tmp[i - 2];
+            i++;
+            return logic(n, tmp, i);
+        }
+    }
 }
